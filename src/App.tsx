48d6891598d4856114b3335fca25bc51793507a0
Youssef Hajjari <youssefhajjari01@gmail.com>
2022-04-11 15:25:16 +0000

    update webpack and main.ts in src folder

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,5 @@
-import React from 'react';
-import { createRoot } from 'react-dom/client';
-import './index.css';
-
 const App = () => {
-  return (
-    <div>
-      <h1 className='font-bold underline text-sm'>Hello world!</h1>
-    </div>
-  );
+  return <h1 className='font-bold underline text-sm'>Hello world!</h1>;
 };
 
-const container = document.getElementById('app');
-const root = createRoot(container);
-root.render(<App />);
+export default App;
