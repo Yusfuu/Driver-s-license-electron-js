eae097ffa07c0147e1e3671e060ab4bda0011daa
FalconHD <youssbak.2015@gmail.com>
2022-04-12 09:42:50 +0000

    aa

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
+import Mamamiya from './components/Mamamiya';
 import './index.css';
 
 const App = () => {
   return (
-    <div>
-      <h1 className='font-bold underline text-sm'>Hello world!</h1>
+    <div className='w-full flex justify-center items-center'>
+      <h1 className=' font-bold underline text-sm'>
+      <Mamamiya />
+      </h1>
     </div>
   );
 };
