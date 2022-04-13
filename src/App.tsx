bdc82c08d3da73ac15c3a5979ea941054a7d465a
FalconHD <youssbak.2015@gmail.com>
2022-04-13 09:51:24 +0000

    merge

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import Login from "./pages/Login";
 import Dashboard from "./pages/Dashboard";
 import Final from "./pages/Final";
@@ -8,32 +7,12 @@ import { FC } from "react";
 import { Home } from "./pages/Home";
 const App: FC = (): JSX.Element => {
   return (
-    <Routes location={"/"}>
+    <Routes >
       <Route path="/" element={<Home />}>
-        <Route path="quiz" element={<Quiz />} />
+        <Route path="/quiz" element={<Quiz />} />
         <Route path="submit" element={<Final />} />
       </Route>
     </Routes>
-=======
-import Login from './pages/Login';
-import Dashboard, { Home } from './pages/Dashboard';
-import Final from './pages/Final';
-import { Routes, Route, useLocation } from 'react-router-dom';
-const App = () => {
-  const { pathname } = useLocation();
-
-  return (
-    <>
-      <Routes location={pathname.replace('index', '')}>
-        <Route path='/' element={<Dashboard />}>
-          <Route path='home' element={<Home />} />
-          <Route path='forms' element={<h1>Forms</h1>} />
-        </Route>
-      </Routes>
-
-      {/* <Final /> */}
-    </>
->>>>>>> ec48b73c081763a46a9966811aa83b85307adef7
   );
 };
 
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -5,7 +5,7 @@ export const Home = () => {
   return (
     <div>
       <h1>quiz</h1>
-      <Link to={"/quiz"}>
+      <Link to="/quiz">
         <button>Quiz</button>
       </Link>
     </div>
